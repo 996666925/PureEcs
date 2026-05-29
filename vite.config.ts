1ac715918ff76d827d5eaf49149519775cf9ccfa
1152207863 <hanweizhao@qcwlcloud.com>
2026-05-29 10:53:12 +0800

    fix: 修复类型声明文件路径

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,15 @@ import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-  plugins: [dts()],
+  plugins: [
+    dts(),
+  ],
   build: {
     lib: {
       entry: 'src/index.ts',
       name: 'PureEcs',
-      formats: ['es', 'cjs'],
-      fileName: (format) => `pureecs.${format === 'es' ? 'mjs' : 'cjs'}`,
+      formats: ['es'],
+      fileName: (format) => `pureecs.mjs`,
     },
     rollupOptions: {
       external: [],
